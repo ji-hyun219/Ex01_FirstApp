6a4465151e217ae8df0d8a7c5d353ec751c4ad5b
ji-hyun219 <ix219@hanmail.net>
2023-03-27 23:11:14 +0900

    feat(ImageView): 이미지 fit, 아이콘, 이미지 둥글게

--- Ex01_FirstApp/ContentView.swift
+++ Ex01_FirstApp/ContentView.swift
@@ -5,42 +5,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            VStack {
-                Spacer()
-                Color.yellow
-                    .frame(height: 100)
-                Spacer()
-                Color.red
-                    .frame(height: 100)
-                Spacer()
-            }
-            HStack() {
-                Spacer()
-                Color.yellow
-                    .frame(width: 100)
-                Spacer()
-                Color.red
-                    .frame(width: 100)
-                Spacer()
-            }
-            VStack {
-                Divider()
-                Color.yellow
-                    .frame(height: 100)
-                Divider()
-                Color.red
-                    .frame(height: 100)
-                Divider()
-            }
+            //가로든 세로든 이미지가 큰 쪽이 테두리에 닿으면 크기가 정해짐.
+            Image("waterfall")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Divider()
+            
+            Image("waterfall2")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 250.0, height: 250.0, alignment: .top)
+            
+            Image(systemName: "play.circle")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(Color.red)
+            
+            Divider()
+            
+            Text("WaterFall")
+                .background( Image("waterfall")
+                                .resizable()
+                                .frame(width: 200, height: 200, alignment: .center) )
+                .frame(width: 200, height: 200, alignment: .center)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .shadow( radius: 10 )
+                .overlay(Circle().stroke(Color.red, lineWidth: 5))
+                
+            
         }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            ContentView()
-            ContentView()
-        }
+        ContentView()
     }
 }
