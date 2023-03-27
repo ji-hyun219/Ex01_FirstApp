f7e4973f956d2b79dc21e3be451c6e6be51cffb9
ji-hyun219 <ix219@hanmail.net>
2023-03-27 21:16:05 +0900

    feat(Preview&DarkMode): 기종별 미리보기 & 다크모드 설정법

--- Ex01_FirstApp/ContentView.swift
+++ Ex01_FirstApp/ContentView.swift
@@ -11,21 +11,39 @@ import SwiftUI
 
 // View 프로토콜: 화면이 보이는 요소
 struct ContentView: View {
+    // Environment 속성래퍼: 환경설정을 읽어오는 어노테이션
+    @Environment(\.colorScheme) var colorScheme
+    
+    
+    
     // some 예약어: View 프로토콜을 준수하는 모든 뷰에 대한 불투명(관용적인) 타입 허용
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, swiftUI!")
-        }
-        .padding()
+        Text("Hello, swiftUI!")
+            .padding(/*@START_MENU_TOKEN@*/.all, 30.0/*@END_MENU_TOKEN@*/)
+            // 배경 색상
+            .background(colorScheme == .light ? Color.white : Color.black)
+            // 콘텐츠 색상
+            .foregroundColor(colorScheme == .light ? Color.black : Color.white)
+            
+            
     }
 }
 
 // 미리보기 화면 정의
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            // 기본 모드
+            ContentView().environment(\.colorScheme, .light)
+            // 다크 모드
+            ContentView().environment(\.colorScheme, .dark)
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+                .previewDisplayName("iPhone 12")
+
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro Max"))
+                .previewDisplayName("iPhone 12 Pro Max")
+        }
     }
 }
