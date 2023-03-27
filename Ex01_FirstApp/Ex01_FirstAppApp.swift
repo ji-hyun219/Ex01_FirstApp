6daca07d28956cee8e42857cce2966efd824baf9
ji-hyun219 <ix219@hanmail.net>
2023-03-27 20:22:32 +0900

    Initial Commit

---
+++ Ex01_FirstApp/Ex01_FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex01_FirstAppApp.swift
+//  Ex01_FirstApp
+//
+//  Created by 이지현 on 2023/03/27.
+//
+
+import SwiftUI
+
+@main
+struct Ex01_FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
