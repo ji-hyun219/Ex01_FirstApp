b69f385aa98ce96a5c71fb426db3ad2448d317a2
ji-hyun219 <ix219@hanmail.net>
2023-04-02 11:25:44 +0900

    project initialize

--- Ex01_FirstApp/ContentView.swift
+++ Ex01_FirstApp/ContentView.swift
@@ -6,38 +6,7 @@ struct ContentView: View {
     @State var sharedData: String = "init value"
     
     var body: some View {
-        VStack {
-            Text("Shared Data : \(self.sharedData)")
-            // $ 를 붙이면, Binding 된 데이터를 전달할 수 있다
-            CustomButton1(sharedData: self.$sharedData)
-            CustomButton2(sharedData: self.$sharedData)
-        }
-    }
-}
-
-struct CustomButton1 : View {
-    @Binding var sharedData: String
-    
-    var body: some View {
-        Button {
-            self.sharedData = "Button1 Clicked!"
-        } label: {
-            Text("Button1")
-        }
-
-    }
-}
-
-struct CustomButton2 : View {
-    @Binding var sharedData: String
-    
-    var body: some View {
-        Button {
-            self.sharedData = "Button2 Clicked!"
-        } label: {
-            Text("Button2")
-        }
-
+        Text("Hello Jihyun!").padding()
     }
 }
 
