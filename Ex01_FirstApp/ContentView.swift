38afe3192a29863f45b50648c95378b1d0a4c09b
ji-hyun219 <ix219@hanmail.net>
2023-04-02 11:22:19 +0900

    feat(State&Binding): Struct 간 데이터 전달

--- Ex01_FirstApp/ContentView.swift
+++ Ex01_FirstApp/ContentView.swift
@@ -3,8 +3,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var sharedData: String = "init value"
+    
     var body: some View {
-        Text("Hello Jihyun").padding()
+        VStack {
+            Text("Shared Data : \(self.sharedData)")
+            // $ 를 붙이면, Binding 된 데이터를 전달할 수 있다
+            CustomButton1(sharedData: self.$sharedData)
+            CustomButton2(sharedData: self.$sharedData)
+        }
+    }
+}
+
+struct CustomButton1 : View {
+    @Binding var sharedData: String
+    
+    var body: some View {
+        Button {
+            self.sharedData = "Button1 Clicked!"
+        } label: {
+            Text("Button1")
+        }
+
+    }
+}
+
+struct CustomButton2 : View {
+    @Binding var sharedData: String
+    
+    var body: some View {
+        Button {
+            self.sharedData = "Button2 Clicked!"
+        } label: {
+            Text("Button2")
+        }
+
     }
 }
 
